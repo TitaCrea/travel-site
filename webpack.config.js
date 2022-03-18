66dddf3d583a1081e364b2823c83a43f301d6b03
Tita <tita.creations@gmail.com>
2022-03-18 13:45:31 +0100

    Completed large hero block and intro content block.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,47 @@
 const path = require( 'path' )
 
+const postCSSPlugins = [
+    // first, via npm we install the plugins we need,
+    // then we list these pkgs here
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+]
+
 module.exports = {
     entry: './app/assets/scripts/app.js',
     output: {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
-    mode: 'development'
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+          },
+          contentBase: path.join(__dirname, 'app'),
+          hot: true,
+          port: 3000,
+          host: '0.0.0.0'
+          },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 
+                    { loader: 'css-loader',
+                        options: { url: false }
+                    },
+                    { loader: 'postcss-loader',
+                        options: { postcssOptions: {
+                                    plugins: postCSSPlugins // we define a variable > see top of the file 'const'
+                                   }
+                        }
+                    }]
+            }
+        ]
+    }
 }
